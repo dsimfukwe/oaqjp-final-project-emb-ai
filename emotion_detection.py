cde3bc826993300a090ce5b3a003dba27662ab5f
= <=>
2025-02-22 14:39:21 -0500

    completed step 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,15 +1,26 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse):
-    '''
-    Analyses text and produces an output of the sentiment expressed
+    '''  Analyses text and produces an output of the sentiment expressed
     '''
     #URL header and payload of the tool
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
-
     #Send a post request
     response = requests.post(url, json=myobj, headers=header)
+    # Parsing the JSON response from the API
+    formatted_response = json.loads(response.text)
+    response_emotions = formatted_response["emotionPredictions"][0]["emotion"]
+    val_max = 0
+    emo_max = ''
+    for (key,val) in response_emotions.items():
+        if val >= val_max:
+            val_max = val
+            emo_max = key
 
-    return response.text
+    emotions_out = response_emotions
+    emotions_out['dominant_emotion'] = emo_max
+
+    return emotions_out
