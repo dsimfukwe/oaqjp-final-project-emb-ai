c6b8d24504eee06dfa308b948e322626a2d04162
= <=>
2025-03-08 14:00:00 -0500

    completed step 7 error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -11,16 +11,26 @@ def emotion_detector(text_to_analyse):
     #Send a post request
     response = requests.post(url, json=myobj, headers=header)
     # Parsing the JSON response from the API
-    formatted_response = json.loads(response.text)
-    response_emotions = formatted_response["emotionPredictions"][0]["emotion"]
-    val_max = 0
-    emo_max = ''
-    for (key,val) in response_emotions.items():
-        if val >= val_max:
-            val_max = val
-            emo_max = key
+    if response.status_code == 400:
+        emotions_out = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+            }
+    else:
+        formatted_response = json.loads(response.text)
+        response_emotions = formatted_response["emotionPredictions"][0]["emotion"]
+        val_max = 0
+        emo_max = ''
+        for (key,val) in response_emotions.items():
+            if val >= val_max:
+                val_max = val
+                emo_max = key
 
-    emotions_out = response_emotions
-    emotions_out['dominant_emotion'] = emo_max
+        emotions_out = response_emotions
+        emotions_out['dominant_emotion'] = emo_max
 
     return emotions_out
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 ''' Executing this function initiates the application of emotion
     analysis to be executed over the Flask channel and deployed on
-    localhost:5000. 
+    localhost:5000.  
 '''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
@@ -14,12 +14,14 @@ def detect_emotion():
     text_to_analyze = request.args.get('textToAnalyze')
     # Pass the text to the emotion_detector function and store the response
     response = emotion_detector(text_to_analyze)
-    
-    # Return a formatted string with the sentiment label and score
-    return f"For the given statement, the system response is \
-    'anger': {response['anger']},'disgust': {response['disgust']},\
-     'fear': {response['fear']}, 'joy': {response['joy']} and \
-     'sadness': {response['sadness']}.The dominant emotion is <b>{response['dominant_emotion']}."
+    if response['dominant_emotion'] == None:
+        return "<b>Invalid text! Please try again!"
+    else:
+        # Return a formatted string with the sentiment label and score
+        return f"For the given statement, the system response is \
+        'anger': {response['anger']},'disgust': {response['disgust']},\
+        'fear': {response['fear']}, 'joy': {response['joy']} and \
+        'sadness': {response['sadness']}.The dominant emotion is <b>{response['dominant_emotion']}."
 
 @app.route("/")
 def render_index_page():
